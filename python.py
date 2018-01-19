0c6039feb8e50bc6826397bd4b37cf5d325b0419
nirusounds <35594546+nirusounds@users.noreply.github.com>
2018-01-19 14:29:51 +0530

    upload

---
+++ python.py
@@ -0,0 +1,23 @@
+number1 = int (input(`Enter  first number:`))
+number2 = int (input(`Enter  second number:`))
+number3 = int (input(`Enter  third number:`))
+
+
+def largest(num1,num2,num3):
+    if (num1 > num2) and (num1 > num3):
+        largest_num = num1
+    elif (num2 > num1) and (num2 > num3):
+        largest_num = num2
+    else:
+        largest_num = num3
+    print("The largest of the 3 numbers is : ", largest_num)
+def smallest(num1, num2, num3):
+    if (num1 < num2) and (num1 < num3):
+        smallest_num = num1
+    elif (num2 < num1) and (num2 < num3):
+        smallest_num = num2
+    else:
+        smallest_num = num3
+    print("The smallest of the 3 numbers is : ", smallest_num)
+largest(number1, number2, number3)
+smallest(number1, number2, number3)
